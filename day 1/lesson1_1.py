6dbcdcf46c86f706fa9c5b2f290daeb250ca4639
nika1keshelava <nika.keshelava.1@btu.edu.ge>
2022-08-29 22:48:26 +0400

    first commit

---
+++ day 1/lesson1_1.py
@@ -0,0 +1,40 @@
+from turtle import *
+
+# turtle-ფაილიდან შემოვიტანოთ ყველა ბრძანება
+speed(10)
+
+# kvadratis daxatva
+color("red")
+shape("turtle")
+begin_fill()
+
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+end_fill()
+
+
+left(90)
+forward(100)
+
+ 
+# saxuravis daxatva
+begin_fill()
+color("green")
+right(30)
+forward(100)
+right(120)
+forward(100)
+
+right(120)
+forward(100)
+
+end_fill()
+
+
+exitonclick()
